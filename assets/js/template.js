0ed7427411a30d56e1bf96eb19252116b3fd2d6b
Unknown <theo.haezebrouck@gmail.com>
2019-08-31 18:21:10 +0200

    Correction URL cards

--- assets/js/template.js
+++ assets/js/template.js
@@ -4,13 +4,16 @@ function test(e, msg) {
 }
 
 function tempateTraduction(text) {
+	
+	const url = encodeURI('http://agri-traduction.fr');
+	console.log(url);
     return `
         <div class="card-item" >
             <div class="card-body">
                 <p class="card-text"> ${ text }</p>
                 <footer>
-                    <a href="  https://www.facebook.com/sharer/sharer.php?u=http://agri-traduction.fr&t=${ text }" class="share facebook" data-network="facebook">Partager sur Facebook</a>
-                    <a href="https://twitter.com/share?&text=${ text }&url=http://agri-traduction.fr&hashtags=hashtag1,hashtag2" class="share twitter" data-network="twitter">Partager sur Twitter</a>
+                    <a href="  https://www.facebook.com/sharer/sharer.php?u=${ url }&t=${ text }" class="share facebook" data-network="facebook">Partager sur Facebook</a>
+                    <a href="https://twitter.com/share?url=${url}&text=${ text }&hashtags='FierDeMonLait'" class="share twitter" data-network="twitter">Partager sur Twitter</a>
                 </footer>
             </div> <!-- / card-body -->
         </div> <!-- / Card-item -->
