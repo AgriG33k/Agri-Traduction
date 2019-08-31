f7b691a4db12fc41fc58bcb142df7051e47043e7
devbieres <devbieres@lafamillebn.net>
2019-08-31 22:32:27 +0200

    Ajout instagram

--- assets/js/template.js
+++ assets/js/template.js
@@ -25,6 +25,7 @@ function tempateTraduction(hashtags, text) {
                   <a href="  https://www.facebook.com/sharer/sharer.php?u=${ url }&t=${ sharedText }" class="share facebook" data-network="facebook"> <img width='32px' src='./assets/img/facebook.png'></a>
                   <!-- <a href="https://twitter.com/share?url=${ url }&text=${ sharedText }&hashtags=${hashtagShared}" class="share twitter" data-network="twitter"><img width='66px' src='./assets/img/twitter.png'></a> -->
                   <a href="https://twitter.com/intent/tweet?text=${ sharedText }&hashtags=${hashtagShared}" class="share twitter" data-network="twitter"><img width='66px' src='./assets/img/twitter.png'></a>
+                  <a href="https://instagram.com" class="share twitter" data-network="twitter"><img src='./assets/img/instagram.png'></a>
                   </p>
                 </footer>
             </div> <!-- / card-body -->
