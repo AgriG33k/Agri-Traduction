6a5671cd1a83f259b8879873bc65991789465035
Fournier <trecy@acta-ds.fr>
2019-08-31 18:27:10 +0200

    Auto stash before merge of "master" and "origin/master"

--- assets/js/template.js
+++ assets/js/template.js
@@ -12,10 +12,10 @@ function tempateTraduction(text) {
             <div class="card-body">
                 <p class="card-text"> ${ text }</p>
                 <footer>
-                  <p>Partager sur  
-                    <a href="  https://www.facebook.com/sharer/sharer.php?u=${ url }&t=${ text }" class="share facebook" data-network="facebook">Partager sur Facebook</a>
-                    <a href="https://twitter.com/share?url=${url}&text=${ text }&hashtags='FierDeMonLait'" class="share twitter" data-network="twitter">Partager sur Twitter</a>
-                  </p>
+                  <p>Partager sur  </p>
+                  <a href="  https://www.facebook.com/sharer/sharer.php?u=http://agri-traduction.fr&t=${ text }" class="share facebook" data-network="facebook"> <img width='32px' src='./assets/img/facebook.png'></a>
+                  <a href="https://twitter.com/share?&text=${ text }&url=http://agri-traduction.fr&hashtags=hashtag1,hashtag2" class="share twitter" data-network="twitter"><img width='66px' src='./assets/img/twitter.png'></a>
+                    
                 </footer>
             </div> <!-- / card-body -->
         </div> <!-- / Card-item -->
