96c2858c8f813c87ac04c7b72ae77a26f2695905
devbieres <devbieres@lafamillebn.net>
2019-09-01 08:15:57 +0200

    La sélection fonctionne

--- assets/js/voice.js
+++ assets/js/voice.js
@@ -1,26 +1,42 @@
+// Récupération des libs
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
 
+// Initialisation des éléments
+var grammar = '#JSGF V1.0; grammar phrase;';
+var  recognition = new SpeechRecognition();
+var speechRecognitionList = new SpeechGrammarList();
+speechRecognitionList.addFromString(grammar, 1);
+recognition.grammars = speechRecognitionList;
+recognition.lang = 'fr-FR';
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
 
-// // Transformation de la zone en select
-// $(document).ready(function() {
+// Gestion du bouton
+var testBtn = document.querySelector('#mic');
+testBtn.addEventListener('click', function () {
+    recognition.start();
+    console.log('Ready to receive a color command.');
+});
 
-//     // Récupération des librairies
-//     const SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
-//     const SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
-//     const SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+// Gestion de l'écoute
+recognition.onresult = function (event) {
+    const speechResult = event.results[0][0].transcript.toLowerCase();
+    console.log('Speech received: ' + speechResult + '.');
 
-//     console.log('Voice');
-//     // Récupération du bouton
-//     const btnEcouter = document.querySelector('#btnEcouter');
-//     btnEcouter.addEventListener('click', ecouterEtRepondre());
+    // Recherche de la phrase dans la liste
+    const ids = sources.filter(item => item.source.toLowerCase() == speechResult );
+    console.log(ids);
+
+    if(ids.length > 0) {
+        $('#source').val(ids[0].code);
+        $('#source').trigger('change');
+    }
     
-// });
+}
 
-
-
-// /**
-//  * Ecoute et répond
-//  */
-// function ecouterEtRepondre() {
-//     console.log('Ecouter !!!');
-// }
-
+// Fin de l'écoute
+recognition.onspeechend = function () {
+    recognition.stop();
+}
