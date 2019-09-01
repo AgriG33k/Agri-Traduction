d94f950d39c97f73565903ebdc6c90fb4fba8c07
devbieres <devbieres@lafamillebn.net>
2019-09-01 07:57:35 +0200

    Version brutale

---
+++ assets/js/voice.js
@@ -0,0 +1,26 @@
+
+
+// // Transformation de la zone en select
+// $(document).ready(function() {
+
+//     // Récupération des librairies
+//     const SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+//     const SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+//     const SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+
+//     console.log('Voice');
+//     // Récupération du bouton
+//     const btnEcouter = document.querySelector('#btnEcouter');
+//     btnEcouter.addEventListener('click', ecouterEtRepondre());
+    
+// });
+
+
+
+// /**
+//  * Ecoute et répond
+//  */
+// function ecouterEtRepondre() {
+//     console.log('Ecouter !!!');
+// }
+
